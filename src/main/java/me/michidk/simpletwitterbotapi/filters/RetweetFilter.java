0291198540eaeca81f36e31af1557b408441bd1e
Michael Lohr <michidk@live.de>
2017-06-19 09:44:38 +0200

    Added retweet filter (and removed this function from ReplyFilter)

--- src/main/java/me/michidk/simpletwitterbotapi/filters/RetweetFilter.java
+++ src/main/java/me/michidk/simpletwitterbotapi/filters/RetweetFilter.java
@@ -3,15 +3,13 @@ package me.michidk.simpletwitterbotapi.filters;
 import twitter4j.Status;
 
 /**
- * Filters replies - so only public tweets are shown
- *
  * Created by Michael Lohr on 19.06.2017.
  */
-public class ReplyFilter extends Filter {
+public class RetweetFilter extends Filter {
 
     @Override
     public boolean filter(Status tweet) {
-        return tweet.getInReplyToStatusId() != -1;
+        return tweet.isRetweet();
     }
 
 }
